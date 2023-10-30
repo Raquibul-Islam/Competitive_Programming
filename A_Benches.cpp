32b7cad33252dd0fbc8630d3b7097fa0aff1a588
Sazzad <sazzad17@cse.pstu.ac.bd>
2023-10-30 19:20:23 +0600

    30-10-23

---
+++ A_Benches.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll n,m, k;
+    cin >> n >> m;
+    ll sum=m;
+    vector < ll > v;
+    for(int i=0;i<n;i++)
+    {
+        ll a;
+        scanf("%lld",&a);
+        sum+=a;
+        v.push_back(a);
+    }
+    sort(v.rbegin(),v.rend());
+    if(sum%n==0)sum=sum/n;
+    else sum=sum/n +1;
+    if(v[0]>=sum)sum=v[0];
+    cout << sum << " " << v[0]+m << endl;
+}
